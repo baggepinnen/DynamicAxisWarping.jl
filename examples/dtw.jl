5c5dfca470f2ad49fabd7c3e831470b2893911fa
Holger Teichgraeber <hteich@cees-mazama.stanford.edu>
2017-07-07 14:35:31 -0700

    updated all examples

--- examples/dtw.jl
+++ examples/dtw.jl
@@ -6,9 +6,14 @@ pyplot()
 # UCI data repository must be downloaded first, run TimeWarp.download_data()
 data = ucr_traindata("Gun_Point");
 
-y1 = Sequence(data[1,2:end]) # dimensions don't work
-y2 = Sequence(data[2,2:end])
+y1 = Sequence(data[1][:,1]) 
+y2 = Sequence(data[1][:,2])
 
 #dtwplot(y1,y2,clim=[0,1])
 dtwplot(y1,y2)
 savefig("dtw.png")
+
+if is_linux()
+  show()
+end
+
--- examples/fastdtw.jl
+++ examples/fastdtw.jl
@@ -1,8 +1,10 @@
 using TimeWarp
+using TimeWarp.Datasets
 using Plots
 gr()
 
-data,labels = TimeWarp.traindata("50words")
+data,labels = ucr_traindata("50words")
 
-plot([ fastdtw(data[:,i], data[:,i+10], radius)[1] for radius=1:100, i=1:10 ])
-xlabel!("radius")
+plot([ fastdtw(data[:,i], data[:,i+10], radius)[1] for radius=1:100, i=1:10 ], show=true)
+xlabel!("radius") 
+
