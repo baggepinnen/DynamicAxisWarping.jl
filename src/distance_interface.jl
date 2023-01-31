1aef50180fa4138bffcf8cffceaebe83d6549afc
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-01-31 06:54:14 +0100

    document normalizers
    [skip-ci]

--- src/distance_interface.jl
+++ src/distance_interface.jl
@@ -10,7 +10,7 @@ abstract type DTWDistance{D <: Union{Function, Distances.PreMetric}} <: Distance
 - `radius`: The maximum allowed deviation of the matching path from the diagonal
 - `dist`: Inner distance
 - `transportcost` If >1, an additional penalty factor for non-diagonal moves is added.
-- `normalizer`: defaults to `Nothing`
+- `normalizer`: defaults to `Nothing`. Supported options are the normalizers defined in [SlidingDistancesBase.jl](https://github.com/baggepinnen/SlidingDistancesBase.jl)
 
 If the two time-series are of equal length, [`dtw_cost`](@ref) is called, if not, [`dtwnn`](@ref) is called.
 """
